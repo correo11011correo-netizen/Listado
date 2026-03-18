9258819f889417a707694f75b11e253a6e9395c7
correo11011correo-netizen <correo11011correo@gmail.com>
2026-03-18 13:15:27 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz_dVuCZ25U7LVJcyqpCRYzwPY32zblTEoP0MTaPAQZoTQcRp2wUQNrm5mnsjWLFpoO/exec",
+  CLIENT_ID: "527108731574-nvkf2sovotbn1vtq7fnh42o6ujkt9lj7.apps.googleusercontent.com",
+  FOLDER_ID: "1HuZ3XmstWNbF90bwEWxkwup_kyxGYyg_"
+};
